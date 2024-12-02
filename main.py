7656e5c4a06800d7d5bb4daebd3d3b7be6411b57
Lucas Roquet <lucasroquet@protonmail.com>
2024-12-02 15:25:08 +0100

    first push

---
+++ main.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+import configs
+import timm
+import torch
+import torchvision
+
+def main() -> None:
+    raise NotImplementedError("TODO")
+
+if __name__ == "__main__":
+    main()
