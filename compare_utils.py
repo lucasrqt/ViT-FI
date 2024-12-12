88eb4c700b53d2cbc2f1dc8da7222934dec90ed2
Lucas Roquet <lucasroquet@protonmail.com>
2024-12-12 01:16:27 +0100

    profiler

--- compare_utils.py
+++ compare_utils.py
@@ -38,3 +38,49 @@ def compare_classification(
                     errors[i] = (0, 1)
 
     return errors
+
+############################################################################################################
+# -- For Grounding Dino
+
+# Convert from (x, y, w, h) to (x1, y1, x2, y2)
+def xywh_to_xyxy(box: torch.Tensor) -> torch.Tensor:
+    box[:2] -= box[2:] / 2   # (x1, y1) = (x - w/2, y - h/2)
+    box[2:] += box[:2]       # (x2, y2) = (x + w/2, y + h/2)
+    return box
+
+
+# Define the element-wise IoU function
+def calculate_iou(box1: torch.Tensor, box2: torch.Tensor) -> float:
+    box1 = xywh_to_xyxy(box1.clone())
+    box2 = xywh_to_xyxy(box2.clone())
+
+    # Intersection
+    inter_x1 = torch.max(box1[0], box2[0])
+    inter_y1 = torch.max(box1[1], box2[1])
+    inter_x2 = torch.min(box1[2], box2[2])
+    inter_y2 = torch.min(box1[3], box2[3])
+
+    # Compute intersection area
+    inter_area = torch.clamp(inter_x2 - inter_x1, min=0) * \
+        torch.clamp(inter_y2 - inter_y1, min=0)
+
+    # Compute the areas of both boxes
+    box1_area = (box1[2] - box1[0]) * (box1[3] - box1[1])
+    box2_area = (box2[2] - box2[0]) * (box2[3] - box2[1])
+
+    # Compute union area
+    union_area = box1_area + box2_area - inter_area
+
+    # IoU calculation
+    iou = inter_area / union_area
+    return iou
+
+def count_elements(lst):
+    """ Count the number of each key in a dictionnary. """
+    counts = {}
+    for elem in lst:
+        if elem in counts:
+            counts[elem] += 1
+        else:
+            counts[elem] = 1
+    return counts
---
+++ profiler.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+
+import configs
+import model_utils
+from torch.profiler import profile, record_function, ProfilerActivity
+
+MODELS = [
+    configs.VIT_BASE_PATCH16_224,
+    configs.SWIN_BASE_PATCH4_WINDOW7_224,
+]
+
+BATCH_SIZE = 32
+DATASET = "imagenet"
+
+def main() -> None:
+    sort_by_keyword = configs.GPU_DEVICE + "_time_total"
+    activities = [ProfilerActivity.CUDA]
+    for model_name in MODELS:
+        model = model_utils.get_model(model_name, configs.FP32)
+        transforms = model_utils.get_vit_transforms(model, configs.FP32)
+        _, test_loader = model_utils.get_dataset(DATASET, transforms, BATCH_SIZE)
+        model = model.to(configs.GPU_DEVICE)
+
+        inputs, _ = next(iter(test_loader))
+        inputs = inputs.to(configs.GPU_DEVICE)
+
+        with profile(activities=activities, record_shapes=True) as prof:
+            with record_function("model_inference"):
+                model(inputs)
+        
+        print("="*20, model_name, "="*20)
+        print(prof.key_averages().table(sort_by=sort_by_keyword))
+
+
+
+if __name__ == "__main__":
+    main()
